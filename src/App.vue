3a365f865234fe562131de0d62843f6755e221f0
Sean <contact@seanyager.com>
2020-03-02 22:02:36 -0600

    unlimited styles!

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
       <transition name="fade" mode="out-in">
-      <section key="intro" class="centerflex" v-if="!interaction">
+      <section key="intro" class="introRings" v-if="!interaction">
         <img class="rings" @click.prevent="playSound()" src="@/assets/imgs/rings.gif" alt="">
       </section>
       
@@ -20,63 +20,70 @@
                   <img class="headerImg" src="@/assets/imgs/wta-icon.jpg" alt="">
 
                 <section class="podcastDescription">
-        
+                  
+                  <section class="podcastIntro">
                   <hr>
 
                   <h2>A Dungeon World Dramatic Performance Podcast</h2>
+                    <hr>
                     
-                  <hr>
-                  
 
-                  <p>
-                  “Welcome to Amara” is a high production dramatic retelling podcast of a play by post Dungeon World campaign. We are <strong><em>Sean and Andy</em></strong>, a GM/Player duo exploring the unique high fantasy world of Amara.
-                  </p>
+                    <p>
+                    “Welcome to Amara” is a high production dramatic retelling podcast of a play by post Dungeon World campaign. We are <strong><em>Sean (The GM)</em></strong> and <strong><em>Andy (Along for the Ride)</em></strong>, a GM/Player duo exploring the unique high fantasy world of Amara.
+                    </p>
+                      
+                    <img class="imageSize floatR" src="@/assets/imgs/amaranmap.png"/>
+
+                    <p>
+                    Amara is a sword and sorcery realm with carefully crafted lore to uncover, featuring a growing cast of diverse characters of beasts and fast friends.
+                    In Amara, there are humans, living among other humanoids scattered across the realm in settlements and villages and unstable camps.
+                    <img class="imageSize floatL" src="@/assets/imgs/banditspink.jpg"/>
+                    At the neck of a prominent peninsula sits <strong>Eyien</strong> (pronounced YEE-in), it’s the city of cities, home to the <em>Ember King of Amara, King Rense.</em>
+                    The title has been held by countless women and men before him. The Ember King's Rule is just and fair, leading Eyien into its immense prosperity.
+                    </p>
+
+                    <img class="floatR imageSize" src="@/assets/imgs/swordsman.png"/>
+
+                    <p>
+                    Eyien is where <strong>“Welcome to Amara”</strong> begins, it’s the place to be for security and for power in Amara.
+                    Within Eyien resides a group of soldiers and mages known as “The Coterie”.
+                    They’re a group sworn to Loyalty under King Rense, and magically bound to the city.
+                    The Coterie is tasked with defending and supporting their home.
+                    They defend Eyien with impunity, proud to be part of something bigger than themselves. 
+                    </p>
+
+                    <img class="floatL imageSize" src="@/assets/imgs/cursepink.jpg"/>
                     
-                  <img class="imageSize floatR" src="@/assets/imgs/amaranmap.png"/>
+                    <p>
+                    Our main character Ydna (played by Andy) is a low ranking member of this Coterie, roped into a secret quest far beyond his intention that is rife with world ending consequences and threats. 
+                    Ydna is a traveling bard a curious drifter, caught up in a flurry of proving grounds quests from his Coterie captain, Orric, the kings emmissary and conduit. 
+                    Above all else Ydna is on a bardic journey to honor his grandfather, hoping to meet exemplary figures and experience all the realm of Amara 
+                    has to offer. He records most, but not all of his discoveries, in his <em>songbook</em> hoping to someday become part of the legends he's read about so eagerly.
+                    </p>
 
-                  <p>
-                  Amara is a sword and sorcery realm with carefully crafted lore to uncover, featuring a growing cast of diverse characters of beasts and fast friends.
-                  In Amara, there are humans, living among other humanoids scattered across the realm in settlements and villages and unstable camps.
-                  <img class="imageSize floatL" src="@/assets/imgs/banditspink.jpg"/>
-                  At the neck of a prominent peninsula sits <strong>Eyien</strong> (pronounced YEE-in), it’s the city of cities, home to the <em>Ember King of Amara, King Rense.</em>
-                  The title has been held by countless women and men before him. The Ember King's Rule is just and fair, leading Eyien into its immense prosperity.
-                  </p>
+                    <img class="floatL imageSize" src="@/assets/imgs/ydnaQuen.jpg"/>
 
-                  <img class="floatR imageSize" src="@/assets/imgs/swordsman.png"/>
+                    <p>
+                    Along the way we discover much about Amara and the city of Eyien, uncover secrets that threaten the kingdom, and piece together the mysterious forces surrounding this grand city of opportunity and potential in "Welcome to Amara".
+                    </p>
+                  </section>
 
-                  <p>
-                  Eyien is where <strong>“Welcome to Amara”</strong> begins, it’s the place to be for security and for power in Amara.
-                  Within Eyien resides a group of soldiers and mages known as “The Coterie”.
-                  They’re a group sworn to Loyalty under King Rense, and magically bound to the city.
-                  The Coterie is tasked with defending and supporting their home.
-                  They defend Eyien with impunity, proud to be part of something bigger than themselves. 
-                  </p>
-
-                  <img class="floatL imageSize" src="@/assets/imgs/cursepink.jpg"/>
-                  
-                  <p>
-                  Our main character Ydna (played by Andy) is a low ranking member of this Coterie, roped into a secret quest far beyond his intention that is rife with world ending consequences and threats. 
-                  Ydna is a traveling bard a curious drifter, caught up in a flurry of proving grounds quests from his Coterie captain, Orric, the kings emmissary and conduit. 
-                  Above all else Ydna is on a bardic journey to honor his grandfather, hoping to meet exemplary figures and experience all the realm of Amara 
-                  has to offer. He records most, but not all of his discoveries, in his <em>songbook</em> hoping to someday become part of the legends he's read about so eagerly.
-                  </p>
-
-                  <img class="floatL imageSize" src="@/assets/imgs/ydnaQuen.jpg"/>
-
-                  <p>
-                  Along the way we discover much about Amara and the city of Eyien, uncover secrets that threaten the kingdom, and piece together the mysterious forces surrounding this grand city of opportunity and potential in "Welcome to Amara".
-                  </p>
-                
-                  <h1>Season 1 Arrives Mid 2020</h1>
-                  
-                  <img class="center bigImage" src="@/assets/imgs/arri.png"/>
-
-                  <h1>"See You in Amara"</h1>
+                 
                 </section>
 
-
-                <p class="signature">-Sean &amp; Andy</p>
               </main>
+
+              <section class="podcastSignoff">
+                  
+                    <h1>Season 1 Arrives Mid 2020</h1>
+                    
+                    <img class="center bigImage" src="@/assets/imgs/arri.png"/>
+
+                    <h1>"See You in Amara"</h1>
+                    
+                    <p class="signature">-Sean &amp; Andy</p>
+                  
+                </section>
 
               <a class="discordLinkWrap" href="https://discord.gg/82GpmYD">
               <section class="discordLink"> 
@@ -138,7 +145,7 @@
 import "@/assets/styles/global.css";
 
 import "@/assets/styles/default.css";
-//import "@/assets/styles/mobileport.css";
+import "@/assets/styles/mobileport.css";
 // import "@/assets/styles/tablet.css";
 // import "@/assets/styles/desktop.css";
 
