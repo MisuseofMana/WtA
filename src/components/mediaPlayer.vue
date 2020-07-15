c3762e6e8ca00d79d60333663fe308a8ac731000
Sean <contact@seanyager.com>
2020-07-15 17:17:04 -0500

    added media player component

---
+++ src/components/mediaPlayer.vue
@@ -0,0 +1,21 @@
+<template>
+    <section class="media-player">
+<iframe title="Welcome to Amara" id="multi_iframe" src="https://www.podbean.com/media/player/multi?playlist=http%3A%2F%2Fplaylist.podbean.com%2F9027875%2Fplaylist_multi.xml&vjs=1&kdsowie31j4k1jlf913=e87a0904c3a22c324b9087a2c4f17c1bfa3133c3&size=430&skin=13&episode_list_bg=%23ffffff&bg_left=%23490025&bg_mid=%23640018&bg_right=%23490025&podcast_title_color=%23ff0080&episode_title_color=%23ff0080&auto=0&share=1&fonts=Verdana&download=0&rtl=0&show_playlist_recent_number=10&pbad=1" frameborder="0" scrolling="no" width="100%" height="432" allowfullscreen=""></iframe>    
+</section>
+</template>
+
+<script>
+    export default {
+        name: "Listen",
+    }
+</script>
+
+<style scoped>
+.media-player {
+    height:432px;
+    display:flex;
+    align-items:center;
+    justify-content: center;
+}
+
+</style>
